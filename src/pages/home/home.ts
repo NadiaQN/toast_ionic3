a7710b52145ed4997611fe66dcccebcf68416dc8
Nadia Quezada <nadia.veronica.quezada.navarrete@everis.com>
2018-12-20 15:46:35 -0300

    inicializando repo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,63 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { IonicPage, ToastController } from 'ionic-angular';
 
+
+@IonicPage()
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private toastCtrl: ToastController) {
+  }
 
+  toastAlert() {
+    let toast = this.toastCtrl.create({
+      message: 'Algun campo ingresado no es válido, intentalo nuevamente',
+      position: 'top',
+      showCloseButton: true,
+      closeButtonText: 'OK',
+      cssClass: 'toast__alert'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+  }
+
+  toastWarning() {
+    let toast = this.toastCtrl.create({
+      message: 'Solo podrás enviar 1 opinión por día, inténtalo mañana',
+      position: 'middle',
+      showCloseButton: true,
+      closeButtonText: 'OK',
+      cssClass: 'toast__warning'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
+  }
+  
+  toastNotifications() {
+    let toast = this.toastCtrl.create({
+      message: 'Tienes un abono de $120.000',
+      position: 'bottom',
+      showCloseButton: true,
+      closeButtonText: 'OK',
+      cssClass: 'toast__notifications'
+    });
+  
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+  
+    toast.present();
   }
 
 }
